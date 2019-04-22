b24ff69f86a05639f166e243fbdf26bdee5d7308
Jude OkAGU <juud0010@gmail.com>
2019-04-23 00:20:58 +0200

    change linkdin

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -29,7 +29,7 @@ class Header extends Component {
             <hr />
             <ul className="social">
               <li><a href ='https://www.facebook.com/juud.ik?ref=bookmarks' rel="noopener noreferrer" target ="_blank">  <i className="fa fa-facebook" /></a></li>
-              <li><a href ='linkedin.com/in/jude-okagu-6142b913b/' rel="noopener noreferrer" target ="_blank">  <i className="fa fa-linkedin" /></a></li>
+              <li><a href ='https://linkedin.com/in/jude-okagu-6142b913b/' rel="noopener noreferrer" target ="_blank">  <i className="fa fa-linkedin" /></a></li>
               <li><a href ='https://www.instagram.com/jdhova/?hl=nl/' rel="noopener noreferrer" target ="_blank">  <i className="fa fa-instagram" /></a></li>
               <li><a href ='https://join.skype.com/invite/p7UwOM5p2T3s/' rel="noopener noreferrer" target ="_blank">  <i className="fa fa-skype" /></a></li>
               <li><a href ='https://github.com/jdhova/' rel="noopener noreferrer" target ="_blank">  <i className="fa fa-github" /></a></li>
